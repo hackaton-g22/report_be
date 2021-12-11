4cb3d352a4e98de2a803d6ef825f0e99ee7502c5
DragnSlav3 <DragnSlav3@gmail.com>
2021-12-11 14:49:02 -0500

    Actualizado modelo y serializador Casos Cerrados

--- hackcodev/models/caso_cerrado_model.py
+++ hackcodev/models/caso_cerrado_model.py
@@ -1,7 +1,8 @@
-from django.db import models
+from django.db      import models
+from .desaparecidos import Desaparecido
 
 class Caso_Cerrado (models.Model):
     id = models.IntegerField(primary_key=True)
     cc_motivo_desaparicion = models.CharField(max_length=100)
-    cc_fecha_aparicion =  models.DateField()
-    cc_id_desaparecido =  models.ForeignKey(Desaparecido, related_name='casos_cerrados', on_delete=models.CASCADE)
+    cc_fecha_aparicion     =  models.DateField()
+    desaparecido_fk        =  models.ForeignKey(Desaparecido, related_name='caso_cerrado', on_delete=models.CASCADE)
